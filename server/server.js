d93755b89d5a7d9e86665abbf4b3d2066882d3b3
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-11-03 15:01:36 -0300

    Reestruturação do banco e mudança nos schemas

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import app from "../app.js";
 import fullstacksRoutes from "../routes/fullstacksRoutes.js";
-import qaRoutes from "../routes/qaRoutes.js";
 import userRoutes from "../routes/userRoutes.js";
-import uxRoutes from "../routes/uxRoutes.js";
 
 app.listen(4000, () =>
   console.log("App escutando na porta http://localhost:4000")
@@ -13,4 +11,4 @@ app.get("/", (req, res) => {
   res.status(200).send("Testando método GET");
 });
 
-app.use(express.json(), userRoutes, uxRoutes, fullstacksRoutes, qaRoutes);
+app.use(express.json(), userRoutes, fullstacksRoutes);
